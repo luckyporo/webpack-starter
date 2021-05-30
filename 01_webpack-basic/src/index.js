a316fe89d88a11ba4c7db3707d2c7329e7a37a82
luckyporo <565010808@qq.com>
2021-05-30 21:41:47 +0800

    update

---
+++ 01_webpack-basic/src/index.js
@@ -0,0 +1,5 @@
+import { sum } from './lib/math';
+const { priceFormat } = require('./lib/format');
+
+console.log(sum(1, 2));
+console.log(priceFormat());
---
+++ 02_webpack-css/src/lib/element.js
@@ -0,0 +1,7 @@
+import '../css/style.css'
+
+const divEl = document.createElement('div');
+divEl.className = 'title';
+divEl.innerHTML = 'hello webpack';
+
+document.body.appendChild(divEl);
