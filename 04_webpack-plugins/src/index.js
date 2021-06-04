d8d2beacf77f25a98cca05ed1f6e935920414fe3
luckyporo <565010808@qq.com>
2021-06-04 23:19:41 +0800

    update

---
+++ 04_webpack-plugins/src/index.js
@@ -0,0 +1,6 @@
+import { sum } from './lib/math';
+const { priceFormat } = require('./lib/format');
+import './lib/element';
+
+console.log(sum(1, 2));
+console.log(priceFormat());
--- 04_webpack-plugins/webpack.config.js
+++ 04_webpack-plugins/webpack.config.js
@@ -1,7 +1,9 @@
 const path = require('path');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
- 
+const { DefinePlugin } = require('webpack');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
 module.exports = {
   entry: './src/index.js',
   output: {
@@ -36,21 +38,42 @@ module.exports = {
         // ],
         type: 'asset',
         generator: {
-          filename: 'img/[name].[hash:6].[ext]'
+          filename: 'img/[name].[hash:6].[ext]',
         },
         parser: {
           dataUrlCondition: {
-            maxSize: 100 * 1024
-          }
-        }
+            maxSize: 100 * 1024,
+          },
+        },
       },
       {
         test: /\.(woff2?|eot|ttf)$/,
         type: 'asset/resource',
         generator: {
-          filename: 'font/[name].[hash:6].[ext]'
-        }
+          filename: 'font/[name].[hash:6].[ext]',
+        },
       },
     ],
   },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: 'webpack-demo',
+      template: './public/index.html',
+    }),
+    new DefinePlugin({
+      BASE_URL: "'./'",
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: 'public',
+          to: './',
+          globOptions: {
+            ignore: ['**/index.html'],
+          },
+        },
+      ],
+    }),
+  ],
 };
